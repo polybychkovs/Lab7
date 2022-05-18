e6ff5ef819336f09f3b6afda8c927b70a2544a5a
BychkovskajaPolina <polina14ght@mail.ru>
2022-05-18 16:35:58 +0300

    created main

--- src/bsu/rfe/java/group6/lab7/Bychkouskaja/var4/Main.java
+++ src/bsu/rfe/java/group6/lab7/Bychkouskaja/var4/Main.java
@@ -1,8 +1,16 @@
 package bsu.rfe.java.group6.lab7.Bychkouskaja.var4;
 
-public class Main {
+import javax.swing.*;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        SwingUtilities.invokeLater(new Runnable() {
+
+            //
+            public void run() {
+                final MainFrame frame = new MainFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }}
